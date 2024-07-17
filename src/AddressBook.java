66a8b521617acecbb79aa61735650a160909d826
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-17 14:33:14 +0530

    Adding Contact

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class AddressBook implements Comparable<AddressBook>{
     private final String firstName;
     private final String lastName;
@@ -8,6 +10,8 @@ public class AddressBook implements Comparable<AddressBook>{
     private final String phoneNumber;
     private final String email;
 
+    private static final List<AddressBook> contacts = new ArrayList<>();
+
     public AddressBook(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -17,6 +21,8 @@ public class AddressBook implements Comparable<AddressBook>{
         this.zip = zip;
         this.phoneNumber = phoneNumber;
         this.email = email;
+
+        contacts.add(this);
     }
 
 
@@ -73,6 +79,15 @@ public class AddressBook implements Comparable<AddressBook>{
         return this.firstName.compareTo(other.firstName);
     }
 
+    public static List<AddressBook> getAllContacts() {
+        return contacts;
+    }
+
+    public static void addContact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+        AddressBook newContact = new AddressBook(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        contacts.add(newContact);
+    }
+
     public static void main(String[] args) {
 
         AddressBook contact1 = new AddressBook("John", "Doe", "123 Main St", "Anytown", "NY", "12345", "555-1234", "john.doe@example.com");
@@ -90,5 +105,13 @@ public class AddressBook implements Comparable<AddressBook>{
 
         System.out.println("Comparing contact1 to contact2: " + contact1.compareTo(contact2));
         System.out.println("Comparing contact2 to contact3: " + contact2.compareTo(contact3));
+
+        AddressBook.addContact("Bob", "Brown", "321 Pine St", "Another City", "FL", "24680", "555-1357", "bob.brown@example.com");
+
+        System.out.println("\nAll Contacts:");
+        List<AddressBook> allContacts = AddressBook.getAllContacts();
+        for (AddressBook contact : allContacts) {
+            contact.display();
+        }
     }
 }
